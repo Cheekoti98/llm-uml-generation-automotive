d43cdbfe9ab0c678b516b9f734a6e78c072e1bd3
Cheekoti98 <cheekotisoumya@gmail.com>
2026-04-04 01:11:23 +0200

    Update run_demo.py

--- src/run_demo.py
+++ src/run_demo.py
@@ -2,7 +2,6 @@ from src.uml_generation.generate import load_model, generate_uml
 
 if __name__ == "__main__":
     BASE_MODEL = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
-    # Optional: later set this to your Drive checkpoint path if you want
     LORA_CKPT = None
 
     tokenizer, model = load_model(BASE_MODEL, LORA_CKPT)
